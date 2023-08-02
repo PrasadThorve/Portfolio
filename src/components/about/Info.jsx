756a9743eb971d22ea9fb9a470540e0e365b45ef
kgs101 <prasadthorve0@gmail.com>
2023-08-02 18:16:47 +0530

    first commit

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Info = () => {
+  return (
+    <div>
+      <div className="about__info grid">
+        <div className="about__box">
+        <i class="uil uil-award-alt"></i>
+          <h3 className="about__title">Experience</h3>
+            <span className="about__subtitle">
+              3 Internships
+            </span>
+          
+        </div>
+
+        <div className="about__box">
+        <i class="uil uil-suitcase"></i>
+          <h3 className="about__title">Completed</h3>
+            <span className="about__subtitle">
+              5+ projects
+            </span>
+          
+        </div>
+
+        
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Info
